919b40d8523ca67840a0b2ef059b1002a9490ccf
imalexianne <imanirax@yahoo.fr>
2019-02-04 16:36:16 +0200

    make a constructor in the class

---
+++ src/app/request-class/request.ts
@@ -0,0 +1,3 @@
+export class Request {
+  constructor(public avatar_url:string,public login:string,public public_repos:number, public followers:number,public following:number,public location:string, public html_url:string){}
+}
