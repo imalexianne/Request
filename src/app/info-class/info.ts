426559b8c28251f807ffdcc448cfb62e9f8860c1
imalexianne <imanirax@yahoo.fr>
2019-02-05 15:33:22 +0200

    change the class name

--- src/app/info-class/info.ts
+++
@@ -1,3 +0,0 @@
-export class Info {
-  constructor(public avatar_url:string,public login:string,public public_repos, public followers:number,public following:number,public location:string, public html_url:string){}
-}
